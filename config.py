5a2f743dff26b8a266a73116cdd79d8a3a4d748a
Bhaskar-069 <bhaskartejaoffl@gmail.com>
2025-04-17 07:17:38 +0530

    Updated configuration to use Supabase instead of MySQL

--- config.py
+++ config.py
@@ -5,8 +5,6 @@ load_dotenv()
 
 class Config:
     SECRET_KEY = os.getenv('SECRET_KEY', os.urandom(32))
-    MYSQL_HOST = os.getenv('MYSQL_HOST', 'localhost')
-    MYSQL_USER = os.getenv('MYSQL_USER', 'root')
-    MYSQL_PASSWORD = os.getenv('MYSQL_PASSWORD', '1973')
-    MYSQL_DB = os.getenv('MYSQL_DB', 'advice_db')
+    SUPABASE_URL = os.getenv('SUPABASE_URL')
+    SUPABASE_KEY = os.getenv('SUPABASE_KEY')
     COHERE_API_KEY = os.getenv('COHERE_API_KEY', 'dwdlq71agDUBvTOIimXYPbwgbdQlawO3L1fOACfk') 
